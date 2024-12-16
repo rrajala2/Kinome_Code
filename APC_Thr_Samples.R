6d1cc3cf3db2dd3d1af14b379e63950687377be7
rrajala2 <146047190+rrajala2@users.noreply.github.com>
2024-12-16 11:07:30 -0600

    Add files via upload

--- APC_Thr_Samples.R
+++ APC_Thr_Samples.R
@@ -1,9 +1,9 @@
-setwd("C:/Users/rahul/Dropbox (OMRF)/Rahul's Folder/Experiments/2023/2-20-22 Cantley-Thrombin-APC")
+setwd("C:/Users/rahul/OMRF Dropbox/Rahul Rajala/Rahul Manuscripts/Rahul Kinome Letter/VPH/DATA")
 
 library(EnhancedVolcano)
 
-APC <- read.delim("C:/Users/rahul/Dropbox (OMRF)/Rahul's Folder/Experiments/2023/2-20-22 Cantley-Thrombin-APC/APC_Kinases.txt")
-Thr <- read.delim("C:/Users/rahul/Dropbox (OMRF)/Rahul's Folder/Experiments/2023/2-20-22 Cantley-Thrombin-APC/Thrombin_.txt")
+APC <- read.delim("C:/Users/rahul/OMRF Dropbox/Rahul Rajala/Rahul Manuscripts/Rahul Kinome Letter/VPH/DATA/APC_Kinases.txt")
+Thr <- read.delim("C:/Users/rahul/OMRF Dropbox/Rahul Rajala/Rahul Manuscripts/Rahul Kinome Letter/VPH/DATA/Thrombin_Kinases.txt")
 
 #APC_UP <- APC[APC$dominant_direction == 'upregulated set' & APC$upregulated_enrichment_value_log2 > 0.6 & APC$upregulated_adjusted_p_value < 0.05,]
 #APC_DN <- APC[APC$dominant_direction == 'downregulated set' & APC$downregulated_enrichment_value_log2 < -0.6 & APC$downregulated_adjusted_p_value < 0.05,]
@@ -18,9 +18,19 @@ Thr_DN <- Thr[Thr$dominant_enrichment_value_log2 < -0.6 & Thr$dominant_adjusted_
 EnhancedVolcano(APC, lab = APC$kinase , x = 'dominant_enrichment_value_log2', y = 'dominant_adjusted_p_value', pCutoff = 0.05, FCcutoff = 0.6, title = "Kinase Activity (aPC)")
 EnhancedVolcano(Thr, lab = Thr$kinase , x = 'dominant_enrichment_value_log2', y = 'dominant_adjusted_p_value', pCutoff = 0.05, FCcutoff = 0.6, title = "Kinase Activity (Thrombin)")
 
+APC_Y <- read.delim("C:/Users/rahul/OMRF Dropbox/Rahul Rajala/Rahul Manuscripts/Rahul Kinome Letter/VPH/DATA/Tyr-PAR1-aPC.txt")
+Thr_Y <- read.delim("C:/Users/rahul/OMRF Dropbox/Rahul Rajala/Rahul Manuscripts/Rahul Kinome Letter/VPH/DATA/Tyr-PAR1-Thrombin.txt")
+
+APC_Y_UP <- APC_Y[APC_Y$dominant_enrichment_value_log2 > 0.6 & APC_Y$dominant_p_value < 0.1,]
+APC_Y_DN <- APC_Y[APC_Y$dominant_enrichment_value_log2 < -0.6 & APC_Y$dominant_p_value < 0.1,]
+Thr_Y_UP <- Thr_Y[Thr_Y$dominant_enrichment_value_log2 > 0.6 & Thr_Y$dominant_p_value < 0.1,]
+Thr_Y_DN <- Thr_Y[Thr_Y$dominant_enrichment_value_log2 < -0.6 & Thr_Y$dominant_p_value < 0.1,]
+
+EnhancedVolcano(APC_Y, lab = APC_Y$kinase , x = 'dominant_enrichment_value_log2', y = 'dominant_p_value', pCutoff = 0.1, FCcutoff = 0.6, title = "Kinase Activity (aPC)")
+EnhancedVolcano(Thr_Y, lab = Thr_Y$kinase , x = 'dominant_enrichment_value_log2', y = 'dominant_p_value', pCutoff = 0.1, FCcutoff = 0.6, title = "Kinase Activity (Thrombin)")
 
 
-write.csv(x = APC_UP, file ="APC_UP.csv")
-write.csv(x = APC_DN, file ="APC_DN.csv")
-write.csv(x = Thr_UP, file ="Thr_UP.csv")
-write.csv(x = Thr_DN, file ="Thr_DN.csv")
+write.csv(x = APC_Y_UP, file ="APC_Y_UP.csv")
+write.csv(x = APC_Y_DN, file ="APC_Y_DN.csv")
+write.csv(x = Thr_Y_UP, file ="Thr_Y_UP.csv")
+write.csv(x = Thr_Y_DN, file ="Thr_Y_DN.csv")
